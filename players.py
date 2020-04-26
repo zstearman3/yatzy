c6b18eefb9b75f1db1763751f5a57b9ee4134598
Zach Stearman <zstearman3@gmail.com>
2020-04-26 15:37:59 -0500

    Add Human and Bot Player classes

--- players.py
+++ players.py
@@ -1,6 +1,20 @@
 import random
 from scoresheets import YatzyScoresheet
 
+BOT_NAMES = [
+    'Jon Snow',
+    'Daenerys Targaryen',
+    'Arya Stark',
+    'Sansa Stark',
+    'Khal Drogo',
+    'Gregor Clegane',
+    'Tyrion Lannister',
+    'Cersei Lannister',
+    'Ramsay Bolton',
+    'Theon Grejoy',
+    'Brienne of Tarth'
+]
+
 class Player():
     def __init__(self, order):
         self.order = order
@@ -16,6 +30,22 @@ class Player():
 
     def score(self):
         return self.scoresheet
+
+class HumanPlayer(Player):
+    def __init__(self, order, name=None):
+        super().__init__(order)
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class BotPlayer(Player):
+    def __init__(self, order):
+        super().__init__(order)
+        self.name = random.choice(BOT_NAMES)
+
+    def __str__(self):
+        return self.name
 
 class PlayerScoresheet():
     pass
