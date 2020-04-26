2e8c86a53606d02524dcdd105bd921e3c96b5122
Zach Stearman <zstearman3@gmail.com>
2020-04-25 20:17:10 -0500

    Add comparisons to Dice class

--- dice.py
+++ dice.py
@@ -10,6 +10,27 @@ class Die:
             raise ValueError("Value must be a whole number")
         self.value = value or random.randint(1, sides)
 
+    def __int__(self):
+        return self.value
+
+    def __eq__(self, other):
+        return int(self) == other
+
+    def __ne__(self, other):
+        return not int(self) == other
+
+    def __gt__(self, other):
+        return int(self) > other
+
+    def __lt__(self, other):
+        return int(self) < other
+
+    def __ge__(self, other):
+        return int(self) > other or int(self) == other
+
+    def __le__(self, other):
+        return int(self) < other or int(self) == other
+
 class D6(Die):
     def __init__(self, value=0):
         super().__init__(sides=6, value=value)
